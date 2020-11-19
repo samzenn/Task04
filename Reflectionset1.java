27dfdfb2a9a1c4ad4aeca681f2ca9bd170643ddc
samzenn <sadaruwan.t@gmail.com>
2020-11-20 00:00:19 +0530

    Create an instance of Reflectionapi class and print it out

---
+++ Reflectionset1.java
@@ -0,0 +1,13 @@
+
+public class Reflectionset1 {
+	
+	
+	public static void main(String[] args)
+
+	 {
+	    Reflectionapi t1 = new Reflectionapi();
+	    
+	System.out.println("t1=" + t1);
+
+	 }
+}
