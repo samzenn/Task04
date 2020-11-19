e9e255f4d17997f3e8dbd36ed245155f3baec06f
samzenn <sadaruwan.t@gmail.com>
2020-11-19 23:55:11 +0530

    Created a Reflectionapi class with similar features of sample class

---
+++ Reflectionapi.java
@@ -0,0 +1,58 @@
+public class Reflectionapi{
+	
+	public long x = 30;
+	public long y = 40;
+	
+	public Reflectionapi() {
+	  }
+
+	  
+
+	public Reflectionapi(long x, long y) 
+	{
+	    this.x = x;
+	    this.y = y;
+	  }
+	
+//Square value of X
+	public void squareX() 
+	{
+	    this.x *= this.x;
+	  }
+
+//Square value of Y
+	public void squareY() 
+	{
+	    this.y *= this.y;
+	  }
+
+//Get X value
+	public long getX() {
+	    return x;
+	  }
+
+	  
+//Set the value
+	private void setA(long x) {
+	    this.x = x;
+	  }
+	
+//Get Y value
+	public long getY() {
+		    return y;
+		  }
+
+		  
+//Set the value
+	private void setY(long y) {
+		    this.y = y;
+		  }
+	
+	
+	public String toString() 
+	{
+	    return String.format("(x:%d, y:%d)", x, y);
+	  }
+
+	
+}
