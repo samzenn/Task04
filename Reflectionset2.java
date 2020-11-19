debda7f58140abbcbffbb189edcec1c2ed864b39
samzenn <sadaruwan.t@gmail.com>
2020-11-20 01:04:32 +0530

    Use of access modifiers and data restricting which methods are accessible outside the class

---
+++ Reflectionset2.java
@@ -0,0 +1,21 @@
+
+public class Reflectionset2 {
+	
+		  public static void main(String[] args) {
+
+		    Reflectionapi s = new Reflectionapi();
+		   
+		 s.squareX();
+		    
+		//s.squareY(); // if you uncomment this you will get a compiler error
+		    
+		//int x = s.x;
+		     
+		//int y = s.y; // if you uncomment this you will get a compiler error
+		    
+		System.out.println("s=" + s);
+		  }
+		
+
+
+}
