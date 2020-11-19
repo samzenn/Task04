8b2383b6b9aa1f3391f0c7fc176592c369a29572
samzenn <sadaruwan.t@gmail.com>
2020-11-20 01:25:33 +0530

    This class demonstrate how to find which class class an object is instance of, getClass returns the object belongs to the class called class and getName method class object call the class name to find out.

---
+++ Reflectionset3.java
@@ -0,0 +1,10 @@
+
+public class Reflectionset3 {
+
+		public static void main(String[] args) {
+			Reflectionapi s = new Reflectionapi();
+			System.out.println("class = " + s.getClass());
+			System.out.println("class name = " + s.getClass().getName());
+		}
+	
+}
